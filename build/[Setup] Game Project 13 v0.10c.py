a9b93bd837a9f510ef593a377dfc7d64340f3291
NightFore <38656758+NightFore@users.noreply.github.com>
2021-09-16 20:22:28 +0200

    Create [Setup] Game Project 13 v0.10c.py

---
+++ build/[Setup] Game Project 13 v0.10c.py
@@ -0,0 +1,16 @@
+import cx_Freeze
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY']  = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+executables = [cx_Freeze.Executable("[Game Project 13] Untitled v0.10c.py")]
+
+cx_Freeze.setup(
+    name="Untitled",
+    options={"build_exe": {"packages": ["pygame"],
+                           "include_files": ["data"]}},
+    executables = executables,
+    version="0.10c"
+
+    )
